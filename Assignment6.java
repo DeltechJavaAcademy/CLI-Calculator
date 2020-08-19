d1375e8f88afdf920cff20a1673b92d6f5915b49
RWLangwig <rlangwig@verizon.net>
2020-08-19 08:41:53 -0400

    Adding 2 decimals to division

--- Assignment6.java
+++ Assignment6.java
@@ -1,64 +1,74 @@
-package CLIcalc;
+// This code will create a command line calculator
+
+package clicalc;
 
 import java.util.*;
 
-public class CLIcalc {
+public class Assignment6 {
 
 	public static void main(String[] args) {
 	
-	Scanner input = new Scanner(System.in);
-	int answer;
-	
-	System.out.println("Welcome to the CLI Calculator");
-	System.out.println("Operations:");
-	System.out.println("1: Addition");
-	System.out.println("2: Subtraction");
-	System.out.println("3: Multiplication");
-	System.out.println("4: Division");
-	System.out.println("");
-	System.out.println("Choose your operation: ");
-	
-	int chosenOp = input.nextInt();
-	
-	if (chosenOp == 1){
-		System.out.println("First Number: ");
-		int num1 = input.nextInt();
-		System.out.println("Second Number: ");
-		int num2 = input.nextInt();
+		Scanner input = new Scanner(System.in);
+		float answer = 0;
 		
-		answer = num1 + num2;
-		System.out.println(num1 + " added to " + num2 + " equals: " + answer);
-	}
-	else if (chosenOp == 2){
-		System.out.println("First Number: ");
-		int num1 = input.nextInt();
-		System.out.println("Second Number: ");
-		int num2 = input.nextInt();
+		System.out.println("Welcome to the CLI Calculator");
+		System.out.println("Operations:");
+		System.out.println("a: Addition");
+		System.out.println("s: Subtraction");
+		System.out.println("m: Multiplication");
+		System.out.println("d: Division");
+		System.out.println("e: Exit");
+		System.out.println("");
+		System.out.println("Choose your operation: ");
 		
-		answer = num1 - num2;
-		System.out.println("Result: " + answer);
-	}
-	else if (chosenOp == 3){
-		System.out.println("First Number: ");
-		int num1 = input.nextInt();
-		System.out.println("Second Number: ");
-		int num2 = input.nextInt();
+		char chosenOp = input.next().charAt(0);
+		chosenOp = Character.toLowerCase(chosenOp);
 		
-		answer = num1 * num2;
-		System.out.println("Result: " + answer);
-	}
-	else if (chosenOp == 4){
-		System.out.println("First Number: ");
-		int num1 = input.nextInt();
-		System.out.println("Second Number: ");
-		int num2 = input.nextInt();
-		
-		if (num2 == 0){
-			System.out.println("Result: Cannot divide by 0.");
-		}
-		else{
-			answer = num1 / num2;
+		if (chosenOp == 'a'){
+			System.out.println("First Number: ");
+			float num1 = input.nextFloat();
+			System.out.println("Second Number: ");
+			float num2 = input.nextFloat();
+			
+			answer = num1 + num2;
 			System.out.println("Result: " + answer);
 		}
-	}
-	}
+		else if (chosenOp == 's'){
+			System.out.println("First Number: ");
+			float num1 = input.nextFloat();
+			System.out.println("Second Number: ");
+			float num2 = input.nextFloat();
+			
+			answer = num1 - num2;
+			System.out.println("Result: " + answer);
+		}
+		else if (chosenOp == 'm'){
+			System.out.println("First Number: ");
+			float num1 = input.nextFloat();
+			System.out.println("Second Number: ");
+			float num2 = input.nextFloat();
+			
+			answer = num1 * num2;
+			System.out.println("Result: " + answer);
+		}
+		else if (chosenOp == 'd'){
+			System.out.println("First Number: ");
+			float num1 = input.nextFloat();
+			System.out.println("Second Number: ");
+			float num2 = input.nextFloat();
+			
+			if (num2 == 0){
+				System.out.println("Result: Cannot divide by 0.");
+			}
+			else{
+			answer = num1 / num2;
+		// display answer to division problem with 2 decimals		
+		    System.out.println("Result: "); 
+		    System.out.printf("%.2f", answer); 
+			}
+		}	
+		else{
+			System.out.println("You did not enter a valid operation. Please start again");
+		}
+		}
+		}
